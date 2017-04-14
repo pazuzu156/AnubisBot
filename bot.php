569a01a8b7fdd8dc8688d7dba46d88657f8ffeff
Kaleb Klein <klein.jae@gmail.com>
2017-04-14 19:14:30 -0400

    Added memory limit increase for bot
    Signed-off-by: Kaleb Klein <klein.jae@gmail.com>

--- bot.php
+++ bot.php
@@ -2,6 +2,8 @@
 
 date_default_timezone_set('America/New_York');
 
+ini_set('memory_limit', '250M');
+
 $GLOBALS['START_TIME'] = time(); // Uptime tacking start
 
 require_once __DIR__.'/vendor/autoload.php';
