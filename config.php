51981c9e1e8ad8600ea5d02e42af8fb11cfc4dbc
Kaleb Klein <klein.jae@gmail.com>
2017-04-18 00:55:52 -0400

    unregistered roles command
    Signed-off-by: Kaleb Klein <klein.jae@gmail.com>

--- config.php
+++ config.php
@@ -10,7 +10,7 @@ return [
         Core\Base\Commands\About::class,
         Core\Base\Commands\Messages::class,
         Core\Base\Commands\Power::class,
-        Core\Base\Commands\Roles::class,
+        // Core\Base\Commands\Roles::class, // Will uncomment when roles are done
         Core\Base\Commands\UserInfo::class,
 
         // Register any of your custom commands here
